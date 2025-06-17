c136866de3bfa6e32a947bb96833561742eb98e4
orionios <117426294+orionios@users.noreply.github.com>
2025-06-17 18:05:25 +0530

    test change

--- src/_dbs/firebase/fbMessage.ts
+++ src/_dbs/firebase/fbMessage.ts
@@ -1,8 +1,8 @@
 import admin from 'firebase-admin';
-import secrate from '../../_configs/fbPrivateKey.json';
+import secrate from '../../fbPrivateKey.json';
 
-let serviceAccount:any = secrate;
-  
+let serviceAccount: any = secrate;
+
 
 export function init() {
     admin.initializeApp({
